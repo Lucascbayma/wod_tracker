68f0fe79d2b1cabe9cdf4fe503b61fe46ec83532
Lucas Bayma <Lucasbayma31@gmail.com>
2025-05-14 19:17:24 -0300

    Add def e organizando o code

--- projeto/main.py
+++ projeto/main.py
@@ -1,27 +1,103 @@
-f=open("Arquivo.txt","a")
-quant=int(input("Digite a quantidade de tipos de treinos que você deseja: "))
 treino=[]
-for i in range(quant):
-   treino.append(input("Digite o treino que deseja realizar:"+"\n"))
-for i in range(quant):
-    print("1-Adicionar algum tipo de exercício\n2-Remover algum treino\n3-Mudar algum treino")
-    n=int(input("Escolha se você quiser fazer algo com sua lista de treino algum tipo de treino da sua lista:"))
-    if n==1:
-        treino.append(input("Digite o treino que você deseja realizar: "))
-    elif n==2:
-        p=int(input("Digite o índice do treino que você deseja excluir: "))
-        treino.pop(p-1)
-    elif n==3:
-        p=int(input("Digite o índice do treino que você deseja modificar: "))
-        indi=p-1
-        if len(treino)<p-1:
-            print("Digite um número correto")
-        else:
-            nv=input("Digite o novo treino que você deseja colocar: ")
-            treino[indi]=nv
+ARQUIVO_TREINOS="treinos.txt"
+ARQUIVO_METAS="metas.txt"
+
+def carregar_treinos():
+    treinos=[]
+    try:
+        with open(ARQUIVO_TREINOS, "r") as lista:
+            for linha in lista:
+                partes=linha.strip().split("|")
+                if len(partes)==4:
+                    treino={
+                        "data": partes[0],
+                        "tipo": partes[1],
+                        "tempo": partes[2],
+                        "movimentos": partes[3]
+                    }
+                    treinos.append(treino)
+    except:
+        pass
+    return treinos
+
+def save_treinos(treinos):
+    with open(ARQUIVO_TREINOS, "w") as lista:
+        for t in treinos:
+            linha = f"{t['data']}|{t['tipo']}|{t['tempo']}|{t['movimentos']}\n"
+            lista.write(linha)
+
+def add_treino():
+    data=input("Data do treino (dd/mm/aaaa): ")
+    tipo=input("Tipo de treino (AMRAP, EMOM, For Time): ").upper()
+    tempo=input("Tempo em minutos (ou For Time): ")
+    movimentos=input("Movimentos (separe por vírgulas): ")
+    
+    treino={"data": data, "tipo": tipo, "tempo": tempo, "movimentos": movimentos}
+    treinos=carregar_treinos()
+    treinos.append(treino)
+    save_treinos(treinos)
+    print("Treino adicionado com sucesso!")
+
+def visu_treinos():
+    treinos=carregar_treinos()
+    if not treinos:
+        print("Você não tem nenhum treino registrado.")
+    for i, t in enumerate(treinos, 1):
+        print(f"{i}. {t['data']} | {t['tipo']} | {t['tempo']} | Movimentos: {t['movimentos']}")
+
+def remover_treino():
+    visu_treinos()
+    i=int(input("Digite o número do treino para remover: "))-1
+    treinos=carregar_treinos()
+    if 0<=i<len(treinos):
+        treinos.pop(i)
+        save_treinos(treinos)
+        print("O seu treino foi removido!")
     else:
-        print("Essa ação não existe!")
-      
-for i in treino:
-   f.write(i + "\n")
-print(treino)
+        print("Índice inválido.")
+
+def editar_treino():
+    visu_treinos()
+    i=int(input("Digite o número do treino para editar: "))-1
+    treinos=carregar_treinos()
+    if 0<=i<len(treinos):
+        print("Deixe vazio para manter o valor atual.")
+        novo_tipo=input("Novo tipo: ")
+        novo_tempo=input("Novo tempo: ")
+        novos_mov=input("Novos movimentos: ")
+        if novo_tipo:
+            treinos[i]['tipo']=novo_tipo.upper()
+        if novo_tempo:
+            treinos[i]['tempo']=novo_tempo
+        if novos_mov:
+            treinos[i]['movimentos']=novos_mov
+        save_treinos(treinos)
+        print("O seu treino foi editado!")
+    else:
+        print("Índice inválido.")
+
+def menu():
+    while True:
+        print("\n----- MENU WOD TRACKER -----")
+        print(" 1-Adicionar treino\n 2-Visualizar treino(s)\n 3-Editar treino\n 4-Remover treino\n 5-Filtrar treinos\n 6-Registrar meta\n 7-Ver metas\n 8-Sugerir WOD\n 0-Sair")
+        try:
+            n=int(input("Escolha uma das opções acima: "))
+
+            if n==1:
+                add_treino()
+            elif n==2:
+                visu_treinos()
+            elif n==3:
+                editar_treino()
+            elif n==4:
+                remover_treino()
+            else:
+                print("Essa opção não existe!")
+            
+            opcao=input("\nVocê deseja continuar ou sair? ")
+            if opcao.lower()=='sair':
+                print("Seu programa foi encerrado com sucesso!")
+                break
+        except ValueError:
+            print("Digite apenas números!")
+menu()
